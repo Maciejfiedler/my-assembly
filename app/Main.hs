3a0e4413e01229b7d63dbcc53e702cf61a977c14
Maciej Fiedler <maciekfiedler@gmail.com>
2021-09-08 20:38:05 +0200

    instruction will get broken down to tripple tuple of strings

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,20 @@
 module Main where
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as B8
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as B8
 
 main :: IO ()
 main = do
     bsFile <- B.readFile "instruction.brb" :: IO B.ByteString
-    print $ stringElem "ADD" bsFile
+    print $ getExpression bsFile
 
-stringElem :: String -> B.ByteString -> [Bool]
-stringElem (x:xs) bs = B.elem x (B.head bs) : stringElem xs (B.tail bs)
+type Expression = (String, String, String) -- function, 1 argument, 2 argument
+
+getExpression :: B.ByteString -> Expression
+getExpression bs = (func, a, b)
+    where 
+        bsInWords = B8.words bs :: [B.ByteString]
+        (func:a:b:_) = bytestringListToString bsInWords
+        
+bytestringListToString :: [B.ByteString] -> [String]
+bytestringListToString [] = []
+bytestringListToString (x:xs) = (B8.unpack x):bytestringListToString xs
