ebe5e8bdb1de54e85ac493b271d2e6e5967e109e
Maciej Fiedler <maciekfiedler@gmail.com>
2021-09-09 22:25:03 +0200

    removed everything and used string instead of bytestring

--- app/Main.hs
+++ app/Main.hs
@@ -1,53 +1,15 @@
 module Main where
-import Data.Either
-import Data.Maybe
-import qualified Data.ByteString.Lazy as B
-import qualified Data.ByteString.Lazy.Char8 as B8
-import qualified Data.Map as M
-
-type Arg = B.ByteString
-type Func = B.ByteString
-type Result = B.ByteString 
-type Expr = (Func, Arg, Arg)
---type Function = (Int -> Int . )
 
 main :: IO ()
 main = do
-    bsFile <- B.readFile "instruction.brb" :: IO B.ByteString
-    let bsLines = B8.lines bsFile
-    let bsInWords = B8.words $ head bsLines -- run only first line
-    let result = runFunction bsInWords
-    B.putStr result
+    instructionFile <- readFile "instruction.brb" :: IO String
+    let instructionLines = lines instructionFile :: [String] 
+    let results = runInstructions instructionLines
+    print results
 
-runFunction :: [B.ByteString] -> B.ByteString
-runFunction bs = either B8.pack processFunction exprEither
-    where 
-        exprEither = seekExpr bs
+runInstructions :: [String] -> [String]
+runInstructions [] = []
+runInstructions (x:xs) = (runInstruction x):(runInstructions xs)
 
-processFunction :: Expr -> B.ByteString -- needs more work
-processFunction (func, a, b) = 
-    case haskellFunc of 
-        Just actualFunc -> B8.pack $ show $ (+) arg1 arg2 
-        Nothing -> (B8.pack "couldn't find function")
-    where
-        haskellFunc = M.lookup func exprList
-        arg1 = read $ B8.unpack a :: Int
-        arg2 = read $ B8.unpack b :: Int
-
-
-seekExpr :: [B.ByteString] -> Either String Expr
-seekExpr bs =
-    if length bs >= 3 then
-        Right (func,a,b)
-    else
-        Left "too few arguments"
-    where (func:a:b:_) = bs
-
-exprList :: M.Map B.ByteString  B.ByteString 
-exprList = M.fromList exprInTuple
-
-exprInTuple :: [(B.ByteString, B.ByteString)]
-exprInTuple = map (\(x,y) -> (B8.pack x, B8.pack y)) list
-    where 
-        list = [
-            ("ADD", "(+)")] :: [(String,String)]
+runInstruction :: String -> String
+runInstruction = id
