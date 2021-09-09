5bb28d967257a7baf5f4657c1ba5c9d3813c6932
Maciej Fiedler <maciekfiedler@gmail.com>
2021-09-09 19:54:15 +0200

    reading an instruction and processing it in haskell (only ADD works) (additation is hard coded (line:30))

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 import Data.Either
+import Data.Maybe
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Lazy.Char8 as B8
 import qualified Data.Map as M
@@ -8,19 +9,31 @@ type Arg = B.ByteString
 type Func = B.ByteString
 type Result = B.ByteString 
 type Expr = (Func, Arg, Arg)
+--type Function = (Int -> Int . )
 
 main :: IO ()
 main = do
     bsFile <- B.readFile "instruction.brb" :: IO B.ByteString
     let bsLines = B8.lines bsFile
-    let bsInWords = B8.words $ head bsLines -- first line
+    let bsInWords = B8.words $ head bsLines -- run only first line
     let result = runFunction bsInWords
     B.putStr result
 
 runFunction :: [B.ByteString] -> B.ByteString
-runFunction bs = 
+runFunction bs = either B8.pack processFunction exprEither
     where 
-        expr = seekExpr bs
+        exprEither = seekExpr bs
+
+processFunction :: Expr -> B.ByteString -- needs more work
+processFunction (func, a, b) = 
+    case haskellFunc of 
+        Just actualFunc -> B8.pack $ show $ (+) arg1 arg2 
+        Nothing -> (B8.pack "couldn't find function")
+    where
+        haskellFunc = M.lookup func exprList
+        arg1 = read $ B8.unpack a :: Int
+        arg2 = read $ B8.unpack b :: Int
+
 
 seekExpr :: [B.ByteString] -> Either String Expr
 seekExpr bs =
@@ -37,4 +50,4 @@ exprInTuple :: [(B.ByteString, B.ByteString)]
 exprInTuple = map (\(x,y) -> (B8.pack x, B8.pack y)) list
     where 
         list = [
-            ("ADD", "add")] :: [(String,String)]
+            ("ADD", "(+)")] :: [(String,String)]
